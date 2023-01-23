f24466a97a8221285836d612b06f9bc89d730e5b
Paulo Victor <paulo.v.prado@hotmail.com>
2023-01-23 19:51:27 -0300

    requisito 4

--- src/routers/productsRoute.js
+++ src/routers/productsRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const productsController = require('../controllers/productsController');
+const { validationProducts } = require('../middlewares/validationProducts');
 
 const productRoute = express.Router();
 
 productRoute.get('/', productsController.getAll);
 productRoute.get('/:id', productsController.getById);
-productRoute.post('/', productsController.insert);
+productRoute.post('/', validationProducts, productsController.insert);
 
 
 module.exports = productRoute;
